ba8dbc3590ac92470579661a00ea8918984509a7
Tima Proje İnşaat <nnxmqtv9nq@privaterelay.appleid.com>
2026-01-24 17:09:14 +0300

    Add Process component with process steps

---
+++ components/Process.js
@@ -0,0 +1,13 @@
+export default function Process() {
+  return (
+    <section className="process">
+      <h2>Süreç Nasıl İlerler?</h2>
+      <ul>
+        <li>Binanı inceliyoruz</li>
+        <li>Belediye uyumlu planlama</li>
+        <li>İnşa süreci</li>
+        <li>Güvenli teslim</li>
+      </ul>
+    </section>
+  );
+}
