9aee11cafc60c3eb0fff3ca0d83bed0ef86e9068
Tima Proje İnşaat <nnxmqtv9nq@privaterelay.appleid.com>
2026-01-24 18:53:53 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,25 @@
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import BuildingView from '../components/BuildingView';
-import CostCalculator from '../components/CostCalculator';
-import BrandStrip from '../components/BrandStrip';
-
 export default function Home() {
   return (
-    <>
-      <Header />
-      <Hero />
-      <BrandStrip />
-      <BuildingView />
-      <CostCalculator />
-    </>
+    <div>
+      <div style={{ height: '100vh', background: 'black', color: 'white' }}>
+        HERO
+      </div>
+
+      <div style={{ height: '100vh', background: 'red' }}>
+        HESAPLAMA
+      </div>
+
+      <div style={{ height: '100vh', background: 'blue' }}>
+        KALİTE
+      </div>
+
+      <div style={{ height: '100vh', background: 'green' }}>
+        CEPHE
+      </div>
+
+      <div style={{ height: '100vh', background: 'purple' }}>
+        SÜREÇ
+      </div>
+    </div>
   );
 }
