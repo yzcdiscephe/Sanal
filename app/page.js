52b80c0ca03f5fa00f382feb844a7d9bebc3b078
Tima Proje İnşaat <nnxmqtv9nq@privaterelay.appleid.com>
2026-01-24 16:44:26 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,33 +1,17 @@
-"use client";
-
-import { useState } from "react";
-import LeftPanel from "../components/LeftPanel";
-import BuildingView from "../components/BuildingView";
-import CostCards from "../components/CostCards";
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import BuildingView from '../components/BuildingView';
+import CostCalculator from '../components/CostCalculator';
+import BrandStrip from '../components/BrandStrip';
 
 export default function Home() {
-  const [district, setDistrict] = useState("Fatih");
-  const [area, setArea] = useState(100);
-  const [facade, setFacade] = useState(false);
-
   return (
-    <main className="container">
-      <LeftPanel
-        district={district}
-        setDistrict={setDistrict}
-        area={area}
-        setArea={setArea}
-        facade={facade}
-        setFacade={setFacade}
-      />
-
-      <BuildingView area={area} facade={facade} />
-
-      <CostCards
-        district={district}
-        area={area}
-        facade={facade}
-      />
-    </main>
+    <>
+      <Header />
+      <Hero />
+      <BrandStrip />
+      <BuildingView />
+      <CostCalculator />
+    </>
   );
 }
