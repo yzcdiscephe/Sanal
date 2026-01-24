42e527d5e08a2583d982f022585cb2965f3f7602
Tima Proje İnşaat <nnxmqtv9nq@privaterelay.appleid.com>
2026-01-24 17:10:16 +0300

    Add FooterCTA component with call-to-action

---
+++ components/FooterCTA.js
@@ -0,0 +1,8 @@
+export default function FooterCTA() {
+  return (
+    <section className="footer-cta">
+      <h2>Dairen İçin En Doğru Senaryoyu Konuşalım</h2>
+      <button>Ücretsiz Ön Görüşme</button>
+    </section>
+  );
+}
