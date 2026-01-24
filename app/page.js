fb16f2c66e95c0e49ac2c7f5456ba37bcb42ae58
Tima Proje İnşaat <nnxmqtv9nq@privaterelay.appleid.com>
2026-01-24 18:59:28 +0300

    Refactor Home component to use imported components

--- app/page.js
+++ app/page.js
@@ -1,25 +1,19 @@
+import Hero from '../components/Hero';
+import Calculator from '../components/Calculator';
+import Quality from '../components/Quality';
+import Facade from '../components/Facade';
+import Process from '../components/Process';
+import FooterCTA from '../components/FooterCTA';
+
 export default function Home() {
   return (
-    <div>
-      <div style={{ height: '100vh', background: 'black', color: 'white' }}>
-        HERO
-      </div>
-
-      <div style={{ height: '100vh', background: 'red' }}>
-        HESAPLAMA
-      </div>
-
-      <div style={{ height: '100vh', background: 'blue' }}>
-        KALİTE
-      </div>
-
-      <div style={{ height: '100vh', background: 'green' }}>
-        CEPHE
-      </div>
-
-      <div style={{ height: '100vh', background: 'purple' }}>
-        SÜREÇ
-      </div>
-    </div>
+    <main>
+      <Hero />
+      <Calculator />
+      <Quality />
+      <Facade />
+      <Process />
+      <FooterCTA />
+    </main>
   );
 }
